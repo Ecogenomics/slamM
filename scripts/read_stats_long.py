d0faf30659872807d52dddd4b252f64c0fd3ed91
mjsull <mjsull@gmail.com>
2019-05-21 16:56:28 +1000

    fixed read_stats_long

--- scripts/read_stats_long.py
+++ scripts/read_stats_long.py
@@ -13,7 +13,7 @@ with open(sys.argv[1]) as f:
     for line in f:
         filename_fastq, filename_fast5, read_id, run_id, channel, mux, start_time, duration, num_events, passes_filtering, \
         template_start, num_events_template, template_duration, sequence_length_template, mean_qscore_template, strand_score_template, \
-        median_template, mad_template = line.split()
+        median_template, mad_template, pore_type, experiment_id, sample_id = line.split()
         bin = int(float(start_time)) // 10800
         while len(time_y) <= bin:
             time_y.append([])
