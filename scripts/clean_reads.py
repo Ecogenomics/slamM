80c7bfa009c10538a003ee058a8d453d405a04f1
mjsull <mjsull@gmail.com>
2019-06-27 13:29:14 +1000

    fixed problems with read cleaning

--- scripts/clean_reads.py
+++ scripts/clean_reads.py
@@ -2,21 +2,22 @@ import os, gzip
 
 
 the_dir = snakemake.input[:-5]
-with open(snakemake.output, 'w') as summary:
+with open('barcoded_reads/summary.txt', 'w') as summary:
     summary.write('barcode\tread_no\tread_length\ttotal_length\n')
     for i in os.listdir(the_dir):
-        if i.endswith('.fastq.gz'):
+        if i.endswith('.fastq'):
             count = 0
             tot_length = 0
             read_file = os.path.join(the_dir, i)
-            with gzip.open(read_file, 'rb') as f, gzip.open(read_file[:-8] + 'clean.fastq.gz', 'wb') as o:
-                while l1 != b'':
+            with gzip.open(read_file) as f, gzip.open(read_file[:-5] + 'clean.fastq.gz', 'wt') as o:
+                while l1 != '':
                     l1 = f.readline()
                     l2 = f.readline()
                     l3 = f.readline()
                     l4 = f.readline()
-                    if l2 != b'\n':
+                    if l2.rstrip() != '':
                         o.write(l1 + l2 + l3 + l4)
                         count += 1
                         tot_length += len(l2.rstrip())
+            os.remove(read_file)
             summary.write(i + '\t' + str(count) + '\t' + str(tot_length/count) + '\t' + str(tot_length) + '\n')
